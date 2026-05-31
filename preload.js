04c9b554408d2bd8f8bb0f22d05b59c5b522bf9b
CHUANHU-MIAO <chuanhumiao@Gmail.com>
2026-05-31 13:29:18 +0800

    Add preload.js

---
+++ preload.js
@@ -0,0 +1,27 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  searchFiles: (query) => ipcRenderer.send('search-files', query),
+  cancelSearch: () => ipcRenderer.send('search-cancel'),
+  openFile: (filePath) => ipcRenderer.invoke('open-file', filePath),
+  openFolder: (filePath) => ipcRenderer.invoke('open-folder', filePath),
+  copyFilePath: (filePath) => ipcRenderer.invoke('copy-file-path', filePath),
+  copyFile: (filePath) => ipcRenderer.invoke('copy-file', filePath),
+  minimizeWindow: () => ipcRenderer.invoke('min-window'),
+  closeWindow: () => ipcRenderer.invoke('close-window'),
+  onSearchResult: (callback) => {
+    const handler = (event, data) => callback(data);
+    ipcRenderer.on('search-result', handler);
+    return () => ipcRenderer.removeListener('search-result', handler);
+  },
+  onSearchDone: (callback) => {
+    const handler = (event, data) => callback(data);
+    ipcRenderer.on('search-done', handler);
+    return () => ipcRenderer.removeListener('search-done', handler);
+  },
+  onSearchError: (callback) => {
+    const handler = (event, data) => callback(data);
+    ipcRenderer.on('search-error', handler);
+    return () => ipcRenderer.removeListener('search-error', handler);
+  },
+});
